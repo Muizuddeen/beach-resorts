c3a10353c6f1b4b835fe81eccd49b79a6908a2d8
Muizuddeen <tabishmuiz@gmail.com>
2019-12-12 16:32:07 +0530

    first commit

---
+++ src/pages/Rooms.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Hero from "../components/Hero";
+import {Link} from "react-router-dom";
+import Banner from "../components/Banner";
+import RoomsContainer from "../components/RoomsContainer";
+const Rooms = () =>{
+  return(
+    <>
+    <Hero hero ="roomsHero">
+    <Banner title ="our rooms">
+    <Link to ="/" className ="btn-primary">
+    return home
+    </Link>
+    </Banner>
+    </Hero>
+    <RoomsContainer/>
+</>
+  );
+};
+export default Rooms;
